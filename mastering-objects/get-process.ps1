e81c7d88aee5e2e93877764c843aeadc5c6d3250
Robert R <grafx2g@gmail.com>
2021-08-15 22:03:49 -0700

    created folders

---
+++ mastering-objects/get-process.ps1
@@ -0,0 +1,9 @@
+Write-Output "get process";
+# get processes that begin with the letter 'a'
+Get-Process a*;
+
+Write-Output "get service";
+# get services that begin with the letter 'a'
+Get-Service a*;
+
+
