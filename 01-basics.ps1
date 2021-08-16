a6cc1ea1765abd2e67fe61a35ecccda0669ca631
Robert R <grafx2g@gmail.com>
2021-08-15 19:39:10 -0700

    basics 01

---
+++ 01-basics.ps1
@@ -0,0 +1,9 @@
+# get-help command to look for help
+
+# update help command gets the latest packages supported
+
+# You can view a list of modules that support updatable help by running the following command:
+# Get-Module -ListAvailable | Where-Object HelpInfoUri
+
+# Used to force updates and silently continue on errors
+# Update-Help -Verbose -Force -ErrorAction SilentlyContinue
