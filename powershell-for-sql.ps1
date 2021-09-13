0ea322237666a2225297bf4e8df45677cf438f56
Robert R <grafx2g@gmail.com>
2021-09-13 13:26:04 -0700

    powershell-for-sql

---
+++ powershell-for-sql.ps1
@@ -0,0 +1,21 @@
+#training website - https://www.youtube.com/watch?v=SmHpKgKashg
+
+$PSVersionTable;
+
+# Examples of Command lets
+
+Get-Date;
+New-Item;
+Remove-Module;
+
+# Three Important Cmdlets
+
+Get-Command;   
+ # Powershell's dictionary lookup, this will help you find cmdlets you can use.
+Get-Help;       
+# Powershell has help info baked right in, just like "man" pages in *nix. 
+Get-Member;     
+# Since Powershell is object aware, this cmdlet helps explore the Methods and Properties.
+
+
+
