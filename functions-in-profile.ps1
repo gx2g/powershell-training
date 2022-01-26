f80fd9a1399d33c7a75eb0c8968a3d4eef465f0a
Robert Resendez <grafx2g@gmail.com>
2022-01-25 21:31:07 -0800

    functions-in-profile.ps1

--- functions-in-profile.ps1
+++ functions-in-profile.ps1
@@ -8,3 +8,20 @@ $profile | Get-Member -Type NoteProperty
 if (!(Test-Path -Path $PROFILE)) {
     New-Item -ItemType File -Path $PROFILE -Force
   }
+
+
+  # The profile paths include the following variables
+  
+  # The $PSHOME variable, which stores the installation directory for PowerShell
+  $PSHOME;
+
+  # The $Home variable, which stores the current user's home directory
+  $HOME;
+
+  # Visual Studio Code supports the following host-specific profiles
+
+  # ALL USERS, Current Host - $PSHOME\Microsoft.VSCode_profile.ps1
+  # CURRENT USER, Current Host - $Home\[My ]Documents\PowerShell\Microsoft.VSCode_profile.ps1
+
+  # In PowerShell Help, the "CurrentUser, Current Host" profile is the profile 
+  # most often referred to as "your PowerShell profile".
