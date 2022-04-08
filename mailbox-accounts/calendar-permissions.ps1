a0b95c659e5bfdc90a073a82c205c56878575cdf
Robert R <grafx2g@gmail.com>
2022-04-08 10:52:33 -0700

    edits

--- mailbox-accounts/calendar-permissions.ps1
+++ mailbox-accounts/calendar-permissions.ps1
@@ -1,6 +1,13 @@
+# Checking Identiity of a Calendar based on email address
+Get-MailboxCalendarFolder -Identity user@domain.com:\calendar
+
+Get-MailboxCalendarFolder -Identity user@domain.com:\calendar
 
 # Add permissions to calendar for user setting access rights to author
 Add-MailboxFolderPermission -Identity mailbox@domain.com:\calendar -user user@domain.com -AccessRights Author
 
+Remove-MailboxFolderPermission -Identity user1@domain.com:\calendar -User user2@domain.com
+
+
 # EXOMail check if permission is applied
-Get-EXOMailboxFolder:Permission outofoffice:\calendar
+Get-EXOMailboxFolderPermission outofoffice:\calendar
