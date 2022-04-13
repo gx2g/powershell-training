af6290146c9d3fdd2e6ae1f378f88e4a9271617b
Robert R <grafx2g@gmail.com>
2022-04-13 16:36:33 -0700

    calendar permissions

--- mailbox-accounts/calendar-permissions.ps1
+++ mailbox-accounts/calendar-permissions.ps1
@@ -6,6 +6,9 @@ Get-MailboxCalendarFolder -Identity user@domain.com:\calendar
 # Add permissions to calendar for user setting access rights to author
 Add-MailboxFolderPermission -Identity mailbox@domain.com:\calendar -user user@domain.com -AccessRights Author
 
+# set permissions to calendar for user setting access rights to author only works if value is not null
+Set-MailboxFolderPermission -Identity mailbox@domain.com:\calendar -User user@domain.com -AccessRights Author
+
 Remove-MailboxFolderPermission -Identity user1@domain.com:\calendar -User user2@domain.com
 
 
