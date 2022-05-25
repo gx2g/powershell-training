b65cc48cf7c0c36e8fe87e6901b2ced7cf431293
Robert R <grafx2g@gmail.com>
2022-05-25 09:08:41 -0700

    get-childitem new entries

--- stepbystepWPS3rdBook/search-for-files.ps1
+++ stepbystepWPS3rdBook/search-for-files.ps1
@@ -10,7 +10,6 @@ Get-Childitem -Path c:\ -Recurse
 # Files you can't access through an error making it hard to read so we use the -ErrorAction SilentlyContinue to not be bothered with them. 
 Get-Childitem -Path c:\ -Recurse -ErrorAction SilentlyContinue
 
-
 # searching we use -include (only show me these files) and -exclude (don't show me these files)
 # we use the -force command to find files in temp folders such as Temporary Outlook folder that are hidden. 
 Get-Childitem -Path c:\ -Recurse -Force -ErrorAction SilentlyContinue
@@ -21,15 +20,11 @@ Get-Childitem -Path C:\ -Include *test* -Recurse -ErrorAction SilentlyContinue
 # -include command with file shows only file types with match
 Get-Childitem -Path c:\ -include *test.txt* -File -Recurse -ErrorAction SilentlyContinue
 
-
 # -exclude works like this
 Get-Childitem -Path c:\ -Include *test* -Exclude *.jpg, *.mp3, *.tmp -File -Recurse -ErrorAction SilentlyContinue
 
-
-
 # Great way to find documents and list them for all files 
 Get-ChildItem -Path C:\users\<username>\desktop\ -Include *.doc,*.docx -File -Recurse -ErrorAction SilentlyContinue
-
 
 # Lets set a time so variable so we can use two commands together / pipe which will include all files since that day
 $FindDate = Get-Date -Year 2021 -Month 01 -Day 01
@@ -37,4 +32,8 @@ $FindDate = Get-Date -Year 2021 -Month 01 -Day 01
 # Combine the two together like this using Where Object LastWriteTime greater then $FindDate
 Get-ChildItem -Path c:\users\<username>\desktop\ -include *.doc,*.docx -File -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.LastWriteTime -ge $FindDate}
 
+# Combine the two together using greater then as well as less then operators with function set to 1 = 24hrs
 Get-ChildItem -Path c:\users\<username>\desktop\ -include *.doc,*.docx -File -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.LastWriteTime -ge $FindDate -AND $_.LastWriteTime -le $Finddate.adddays(1)}
+
+# search a list of folders for a file on different drives
+Get-ChildItem -Path c:\users, l:\foldername, x:\foldername\foldername\ -Include filename*.doc? -Recurse
