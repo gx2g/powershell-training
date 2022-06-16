da5427761b04f0a98d19af233373a818324610a7
Robert R <grafx2g@gmail.com>
2022-06-15 23:07:18 -0700

    controlling execution

---
+++ stepbystepWPS3rdBook/controlling-execution.ps1
@@ -0,0 +1,16 @@
+# -WhatIf parameter after cmdlets, this only works for cmdlets thatchange system state such as Set-Service
+
+# There is no -WhatIf parameter for cmdlets like Get-Process that display only information
+
+# Run notepad once running type in powershell
+Get-Process note* # using shorthand search
+Stop-Process -id 10712 -WhatIf
+
+# displays - What if: Performing the operation "Stop-Process" on target "notepad (10712)"
+
+# You can also use -Confirm
+
+Stop-Process -id 10712 -Confirm
+
+# s to suspend execution when using -Confirm parameter
+
