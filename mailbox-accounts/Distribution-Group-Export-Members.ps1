83fddda8f547044652ec15f9a00d0c22c8b815b2
Robert R <grafx2g@gmail.com>
2022-07-28 13:17:09 -0700

    latest changes

--- mailbox-accounts/Distribution-Group-Export-Members.ps1
+++ mailbox-accounts/Distribution-Group-Export-Members.ps1
@@ -15,20 +15,21 @@ Get-DistributionGroup
 
 Get-DistributionGroup -Identity "WF Team" | Format-List
 
+# Obtain a list of group members in group
 Get-DistributionGroupMember -Identity "WF Team" ResultSize Unlimited
 
+# Assign that group to a variable
 $DGName = "WF Team"
 
+# Export out a list of gorup members with Name and Email Addresses
 Get-DistributionGroupMember -Identity $DGName | Select-Object Name, PrimarySMTPAddress | Export-CSV "C:\Distribution-List-Members.csv" -NoTypeInformation -Encoding UTF8
 
-
-## you can check what you have in the CSV you just exported by doing this. 
-
+# you can check what you have in the CSV you just exported by doing this. 
 Get-Content C:\filename.csv | Out-GridView
 
-## ================================================================================================================= ##
-### This works great if you want a single list but lets make one that grabs all groups and creates one big CSV file. 
-## ================================================================================================================= ##
+# ================================================================================================================= ##
+# This works great if you want a single list but lets make one that grabs all groups and creates one big CSV file. 
+# ================================================================================================================= ##
 
 
 
