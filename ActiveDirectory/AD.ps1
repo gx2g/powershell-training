338a44293adfdb8528279a9052d2eecac2b78771
Robert R <grafx2g@gmail.com>
2022-08-24 08:45:17 -0700

    AD

--- ActiveDirectory/AD.ps1
+++ ActiveDirectory/AD.ps1
@@ -1,2 +1,5 @@
-#
-Set-ADUser username -Description 'Sales Team Member'
+# Getting AD Information
+Get-ADDomain;
+
+# Simple Test to edit the discription of an AD User
+Set-ADUser username -Description 'Sales Team Member';
