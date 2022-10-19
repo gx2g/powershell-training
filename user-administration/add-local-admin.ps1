a6a67465014c92a4da0c89a7c371c95bbe2e8e0c
Robert R <grafx2g@gmail.com>
2022-10-19 08:15:56 -0700

    add local admin

--- user-administration/add-local-admin.ps1
+++ user-administration/add-local-admin.ps1
@@ -5,5 +5,22 @@ $LocGroup = "Administrators"
 
 $adsi = [ADSI]"WinNT://$env:COMPUTERNAME"
 
-$existing = $adsi.Cildren | where {$_.SchemaClassName}
+$existing = $adsi.Children | where { $_.SchemaClassName -eq 'user' -and $_.Name -eq $Username }
+
+if ($existing -eq $null) {
+
+    Write-Host "Creating new local user $Username."
+    & NET USER $Username $Password /add /y /expires:never
+
+    Write-Host "Adding local user $Username to $LocGroup."
+    & NET LOCALGROUP $LocGroup $Username /add
+}
+
+else {
+    Write-Host "Setting password for existing local user $Username."
+    $existing.SetPassword($Password)
+}
+
+Write-Host "Ensuring password for $Username never expires."
+& WMIC USERACCOUNT WHERE "Name='$Username'" SET PasswordExpires=FALSE
 
