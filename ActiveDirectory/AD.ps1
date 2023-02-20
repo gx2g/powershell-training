ced69f5264299e27e496d453b11e9a828d66ca3a
Robert R <grafx2g@gmail.com>
2023-02-20 15:10:25 -0800

    Add AD User from Powershell

--- ActiveDirectory/AD.ps1
+++ ActiveDirectory/AD.ps1
@@ -3,3 +3,12 @@ Get-ADDomain;
 
 # Simple Test to edit the discription of an AD User
 Set-ADUser username -Description 'Sales Team Member';
+
+# Create a password object
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Module.$PasswordProfile
+
+# Assign the password
+$PasswordProfile.Password = "<Password>"
+
+# Create the new user
+New-AzureADUser -AccountEnabled $true -DisplayName "Joe Doe" -PasswordProfile $PasswordProfile -MailNickName "JohnD" -UserPrincipalName "johnd@contos.com"
