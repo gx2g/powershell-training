5da6122ed4c48716925e7084c0837953e8522f25
Robert R <grafx2g@gmail.com>
2023-05-03 09:31:52 -0700

    help

---
+++ help/help-reference.ps1
@@ -0,0 +1,30 @@
+# Help Reference
+
+get-help Get-Command;
+
+get-help Get-Verb;
+
+get-help about_aliases;
+
+get-help get-member;
+
+get-help about_Functions;
+
+get-help about_functions_advanced;
+
+get-help about_scripts;
+
+get-help about_execution_policies; 
+
+get-help about_scopes;
+
+get-help about_Pipelines;
+
+get-help where-objects;
+
+get-help about_variables;
+
+get-help about_commonparameters;
+
+get-help about_modules;
+
