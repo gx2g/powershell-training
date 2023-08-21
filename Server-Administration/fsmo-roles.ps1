41af63e08537abd72a6138f10fb2cc17a402f7df
Robert R <grafx2g@gmail.com>
2023-08-21 13:50:39 -0700

    Server Administration PS Commands

---
+++ Server-Administration/fsmo-roles.ps1
@@ -0,0 +1,37 @@
+#############################################################
+
+    # Get-ADDomain
+    # Get-ADTrust -Filter *
+    # Get-ADForest | Select Name, ForestMode
+    # Get-ADDomain | Select Name, DomainMode
+
+#############################################################
+
+# Forest Wide Roles: 
+    # Schema Master
+    # Domain naming master
+
+# Domain Wide Roles: 
+    # PDC
+    # RID pool manager
+    # Infrastructure master
+
+# Method 1: Run cmd and type (netdom query fsmo) 
+
+# Method 2: Powershell
+
+# Returns Forest FSMO Rules
+Get-ADForest domain.local | Format-Table SchemaMaster, DomainNamingMaster;
+
+# Returns Domain FSMO Roles
+Get-ADDomain domain.local | Format-Table PDCEMulator, RIdMaster, InfrastructureMaster;
+
+# Why Transfer FSMO Roles?
+
+# By Default all five FSMO roles are assigned to the first domain controller in the forest root domain. Transfering roles is often needed for a few reasons. 
+    # - Upgrading the operating system
+    # - Changing IP addresses on a domain controller
+    # - Demoting a domain controller
+    # - Taking a domain controller offline for maintenance
+    # - Performance issues
+
