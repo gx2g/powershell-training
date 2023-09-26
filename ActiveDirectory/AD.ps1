de3f7e1d1ba4d6d4428d0ab9da1993b40f9f3fec
Robert R <grafx2g@gmail.com>
2023-09-26 16:50:36 -0700

    AD Group, AD Group Members

--- ActiveDirectory/AD.ps1
+++ ActiveDirectory/AD.ps1
@@ -3,3 +3,13 @@ Get-ADDomain;
 
 # Simple Test to edit the discription of an AD User
 Set-ADUser username -Description 'Sales Team Member';
+
+
+# Find AD Group
+Get-ADGroup -filter * | sort name | select Name
+
+# Use Get-ADGroupMember to list group members
+Get-ADGroupMember -identity "GroupName" | format-list 
+
+# Use Get-ADGroupMember to list group members clean
+Get-ADGroupMember -identity "GroupName" | format-table
