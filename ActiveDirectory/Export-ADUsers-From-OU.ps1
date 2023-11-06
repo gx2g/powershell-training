3daa52963452e641f2a6caa6f676edb6ed33e3ff
Robert R <grafx2g@gmail.com>
2023-11-06 09:28:17 -0800

    Export ADUsers from OU

--- ActiveDirectory/Export-ADUsers-From-OU.ps1
+++ ActiveDirectory/Export-ADUsers-From-OU.ps1
@@ -1,3 +1,3 @@
 $OU = 'OU=Accounts Employees,OU=CTE - Users,DC=CTE,DC=local'
 # Use Get-AdUser to search within organizational unit to get users name
-Get-ADUser -Filter * -SearchBase $OU | Select-object DistinguishedName,Name,UserPrincipalName | Export-Csv -path 'C:\users\Admin\Desktop\CTE-Employee-List-csv' -NoTypeInformation
+Get-ADUser -Filter * -SearchBase $OU | Select-object DistinguishedName,Name,UserPrincipalName | Export-Csv -path 'C:\users\Admin\Desktop\Employee-List-csv' -NoTypeInformation
